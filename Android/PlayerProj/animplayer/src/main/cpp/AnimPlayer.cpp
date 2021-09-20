05d469b4b6bcf66cafbc92949cf8258b8cf7baad
lverniu777 <lverniu777@163.com>
2021-09-20 15:38:21 +0800

    添加AnimPlayer.cpp

---
+++ Android/PlayerProj/animplayer/src/main/cpp/AnimPlayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by huaxia on 2021/9/12.
+//
+
+#include "include/animplayer/AnimPlayer.h"
---
+++ Android/PlayerProj/animplayer/src/main/cpp/include/animplayer/AnimPlayer.h
@@ -0,0 +1,16 @@
+//
+// Created by huaxia on 2021/9/12.
+//
+
+#ifndef PLAYERPROJ_ANIMPLAYER_H
+#define PLAYERPROJ_ANIMPLAYER_H
+
+
+class AnimPlayer {
+private:
+    std::queue<int> q;
+
+};
+
+
+#endif //PLAYERPROJ_ANIMPLAYER_H
